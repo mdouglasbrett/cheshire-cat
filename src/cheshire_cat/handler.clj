ba5eb922cd6d9d0f61bdb79d73e2e2e7c9f97669
Michael Brett <mdouglasbrett@icloud.com>
2017-12-01 08:43:59 +0000

    Fix deps typo

--- src/cheshire_cat/handler.clj
+++ src/cheshire_cat/handler.clj
@@ -2,7 +2,7 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [ring.middleware.json :as json]
+            [ring.middleware.json :as ring-json]
             [ring.util.response :as rr]))
 
 (defroutes app-routes
