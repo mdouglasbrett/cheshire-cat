c4ebab3d1467572ffc6478aae4f6fe7f9844f03c
Michael Brett <mdouglasbrett@icloud.com>
2017-12-06 14:04:48 +0000

    Match tutorial dependencies

--- project.clj
+++ project.clj
@@ -3,16 +3,16 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.1"]
-                 [ring/ring-defaults "0.2.1"]
-                 [ring/ring-json "0.4.0"]
-                 [org.clojure/clojurescript "1.9.946"]]
-  :plugins [[lein-ring "0.9.7"]
-            [lein-cljsbuild "1.1.7"]]
+                 [compojure "1.3.1"]
+                 [ring/ring-defaults "0.1.2"]
+                 [ring/ring-json "0.3.1"]
+                 [org.clojure/clojurescript "0.0-2371"]]
+  :plugins [[lein-ring "0.8.13"]
+            [lein-cljsbuild "1.0.3"]]
   :ring {:handler cheshire-cat.core.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}}
+                        [ring/ring-mock "0.1.5"]]}}
   :cljsbuild {
               :builds [{
                         :source-paths ["src-cljs"]
