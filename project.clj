a7d006b17eb8e58c671dd1a6ccec7d482c4d94d1
Michael Brett <mdouglasbrett@icloud.com>
2017-12-07 09:04:37 +0000

    Comment out clojurescript references
    Might have to nuke this from orbit. Only way to make sure...

--- project.clj
+++ project.clj
@@ -3,20 +3,20 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]
-                 [ring/ring-json "0.3.1"]
-                 [org.clojure/clojurescript "0.0-2371"]]
-  :plugins [[lein-ring "0.8.13"]
-            [lein-cljsbuild "1.0.3"]]
+                 [compojure "1.6.0"]
+                 [ring/ring-defaults "0.3.1"]
+                 [ring/ring-json "0.4.0"]]
+;                 [org.clojure/clojurescript "0.0-2371"]]
+  :plugins [[lein-ring "0.12.2"]]
+;            [lein-cljsbuild "1.0.3"]]
   :ring {:handler cheshire-cat.core.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.1.5"]]}}
-  :cljsbuild {
-              :builds [{
-                        :source-paths ["src-cljs"]
-                        :compiler {
-                                   :output-to "resources/public/main.js"
-                                   :optimizations :whitespace
-                                   :pretty-print :true}}]})
+                        [ring/ring-mock "0.3.2"]]}})
+;  :cljsbuild {
+;              :builds [{
+;                        :source-paths ["src-cljs"]
+;                        :compiler {
+;                                   :output-to "resources/public/main.js"
+;                                   :optimizations :whitespace
+;                                   :pretty-print :true}}]})
