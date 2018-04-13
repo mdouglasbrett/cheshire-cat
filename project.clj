0e6a3cf3cc5388ccda2e34a55295629519ac7f89
Michael Brett <michael.brett.webdev@gmail.com>
2018-04-13 12:05:16 +0100

    Add browser connected REPL, mv cat.html

--- project.clj
+++ project.clj
@@ -5,9 +5,18 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.6.0"]
                  [ring/ring-defaults "0.3.1"]
-                 [ring/ring-json "0.4.0"]]
-  :plugins [[lein-ring "0.12.2"]]
+                 [ring/ring-json "0.4.0"]
+                 [org.clojure/clojurescript "0.0-2371"]]
+  :plugins [[lein-ring "0.12.2"]
+            [lein-cljsbuild "1.0.3"]]
   :ring {:handler cheshire-cat.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring-mock "0.1.5"]]}})
+                        [ring-mock "0.1.5"]]}}
+  :cljsbuild {
+               :builds [{
+                          :source-paths ["src-cljs"]
+                          :compiler {
+                                      :output-to "resources/public/main.js"
+                                      :optimizations :whitespace
+                                      :pretty-print true}}]})
