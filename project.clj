c0bd3158e4208855f0c731bbb6f4133c64e7c19c
Michael Brett <michael.brett.webdev@gmail.com>
2018-04-13 18:41:31 +0100

    Add enfocus and set content

--- project.clj
+++ project.clj
@@ -6,11 +6,12 @@
                  [compojure "1.6.0"]
                  [ring/ring-defaults "0.3.1"]
                  [ring/ring-json "0.4.0"]
-                 [org.clojure/clojurescript "0.0-2371"]
-                 [cljs-http "0.1.18"]
-                 [org.clojure/core.async "0.4.474"]]
-  :plugins [[lein-ring "0.12.2"]
-            [lein-cljsbuild "1.0.3"]]
+                 [org.clojure/clojurescript "1.10.238"]
+                 [cljs-http "0.1.45"]
+                 [org.clojure/core.async "0.4.474"]
+                 [enfocus "2.1.1"]]
+  :plugins [[lein-ring "0.12.4"]
+            [lein-cljsbuild "1.1.7"]]
   :ring {:handler cheshire-cat.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
--- src-cljs/core.cljs
+++ src-cljs/core.cljs
@@ -1,5 +1,17 @@
 (ns cheshire-cat.core
-  (:require [clojure.browser.repl :as repl]))
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [clojure.browser.repl :as repl]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]
+            [enfocus.core :as ef]))
 
 (defn ^:export init []
-  (repl/connect "http://localhost:9000/repl"))
+  (repl/connect "http://localhost:9000/repl")
+
+  (go (let [response (<! (http/get "/cheshire-cat"))
+            body (:body response)]
+        (ef/at "#cat-name" (ef/content (:name body))
+               "#status" (ef/do->
+                          (ef/content (:status body))
+                          (ef/set-style :font-size "500%")))
+        )))
