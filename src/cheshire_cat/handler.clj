a183112a1d3f28bd1f2f5eaa092da6c0e0c65fcf
Michael Brett <michael.brett.webdev@gmail.com>
2018-04-13 10:52:51 +0100

    Add Cheshire library, update routes

--- src/cheshire_cat/handler.clj
+++ src/cheshire_cat/handler.clj
@@ -3,10 +3,17 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.json :as ring-json]
-            [ring.util.response :as rr]))
+            [ring.util.response :as rr]
+            [cheshire.core :as json]))
 
 (defroutes app-routes
   (GET "/" [] "Hello World, this is a clojure app running in a docker container")
+  (GET "/cheshire-cat" []
+       {:status 200
+        :headers {"Content-Type" "application/json; charset=utf-8"}
+        :body (json/generate-string
+               {:name "Cheshire Cat"
+                :status :grinning})})
   (route/not-found "Not found"))
 
 (def app
