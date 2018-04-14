1b61eede40ad0c3dbe2a9aaaa3e6eb3ea1ada614
Michael Brett <michael.brett.webdev@gmail.com>
2018-04-14 12:26:41 +0100

    Add events/effects

--- src-cljs/core.cljs
+++ src-cljs/core.cljs
@@ -3,7 +3,15 @@
   (:require [clojure.browser.repl :as repl]
             [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]
-            [enfocus.core :as ef]))
+            [enfocus.core :as ef]
+            [enfocus.events :as ev]
+            [enfocus.effects :as ee]))
+
+(defn say-goodbye []
+  (ef/at
+   "#cat-name" (ee/fade-out 500)
+   "#button1" (ee/fade-out 500)
+   "#status" (ee/fade-out 5000)))
 
 (defn ^:export init []
   (repl/connect "http://localhost:9000/repl")
@@ -14,4 +22,5 @@
                "#status" (ef/do->
                           (ef/content (:status body))
                           (ef/set-style :font-size "500%")))
-        )))
+        (ef/at "#button1" (ev/listen :click
+                                     say-goodbye)))))
